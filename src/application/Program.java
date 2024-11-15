fa65426ba20ba836cfd183af0cd99ea41a504c89
calebewerneckcouto <calebewerneck@hotmail.com>
2024-11-15 18:16:32 -0300

    salvando dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,45 +3,32 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Triangle;
+import entities.Product;
 
 public class Program {
-
-	public static void main(String[] args) {
-
+      
+	
+	public static void main (String []args) {
 		Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
 		
-	       Triangle x,y;
-	       x= new Triangle();
-	       y = new Triangle();
-	
-		System.out.println("Entre com as medidas do triangulo X:");
-		x.a = sc.nextDouble();
-		x.b = sc.nextDouble();
-		x.c = sc.nextDouble();
-
-		System.out.println("Entre com as medidas do triangulo y:");
-
-		y.a = sc.nextDouble();
-		y.b = sc.nextDouble();
-		y.c = sc.nextDouble();
-
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		product.name = sc.nextLine();
 		
-		double areaX = x.area();
+		System.out.println("Price: ");
+		product.price = sc.nextDouble();
 		
-		double areay = y.area();
-
-		System.out.printf("Triangulo X  area : %.4f%n", areaX);
-		System.out.printf("Triangulo y  area : %.4f%n", areay);
-
-		if (areaX > areay) {
-			System.out.println("X tem area maior");
-		} else {
-			System.out.println("Y tem area maior");
-
-		}
-
+		System.out.println("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product);
+		
+		
+		sc.close();
+		
+		
 	}
-
 }
---
+++ src/application/ProgramTriangle.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
+
+public class ProgramTriangle {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	       Triangle x,y;
+	       x= new Triangle();
+	       y = new Triangle();
+	
+		System.out.println("Entre com as medidas do triangulo X:");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+
+		System.out.println("Entre com as medidas do triangulo y:");
+
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+
+		
+		double areaX = x.area();
+		
+		double areay = y.area();
+
+		System.out.printf("Triangulo X  area : %.4f%n", areaX);
+		System.out.printf("Triangulo y  area : %.4f%n", areay);
+
+		if (areaX > areay) {
+			System.out.println("X tem area maior");
+		} else {
+			System.out.println("Y tem area maior");
+
+		}
+
+	}
+
+}
